6e5ae97346e88dd6f11b78571fe27a4e11b46b4c
Priyobrato Das <priyodas12@gmail.com>
2020-07-26 23:14:43 +0530

    Initial commit

---
+++ src/basic/RegExImplOne.java
@@ -0,0 +1,30 @@
+package basic;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegExImplOne {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        System.out.println("Enter target string:::");
+        String targetString=scn.next();
+        System.out.println("Enter regex string:::");
+        String findRegexString=scn.next();
+
+        int countOfRegEx=0;
+
+        //which regular expression we are searching pattern
+        Pattern pattern=Pattern.compile(findRegexString);
+
+        //target String
+        Matcher matcher=pattern.matcher(targetString);
+
+        //
+        while(matcher.find()){
+            countOfRegEx++;
+        }
+
+        System.out.println("'"+findRegexString+"' presents  "+countOfRegEx+ " times in "+targetString);
+    }
+}
