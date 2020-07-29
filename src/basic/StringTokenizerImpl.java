d7367e92a2c68a540f5e827c9da61104517f5f55
Priyobrato Das <priyodas12@gmail.com>
2020-07-29 23:44:33 +0530

    string programs

--- src/basic/StringTokenizerImpl.java
+++ src/basic/StringTokenizerImpl.java
@@ -5,10 +5,15 @@ import java.util.StringTokenizer;
 public class StringTokenizerImpl {
     public static void main(String[] args) {
         //default regex for string tokenizer is space
-        StringTokenizer st=new StringTokenizer("www infosys com");
+        StringTokenizer st1=new StringTokenizer("www infosys com");
+        StringTokenizer st2=new StringTokenizer("07/29/2020","/");
 
-        while(st.hasMoreTokens()){
-            System.out.println(st.nextToken());
+        while(st1.hasMoreTokens()){
+            System.out.println(st1.nextToken());
+        }
+
+        while(st2.hasMoreTokens()){
+            System.out.println(st2.nextToken());
         }
     }
 }
